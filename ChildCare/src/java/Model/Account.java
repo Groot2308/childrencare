1d9555c902dbee11cf22a44aa477cb4f31786a46
Groot2308 <danghoan2382002@gmail.com>
2023-10-02 00:33:38 +0700

    child

---
+++ ChildCare/src/java/Model/Account.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ *
+ * @author mihxdat
+ */
+@Builder
+
+public class Account {
+    private int id;
+    private String email;
+    private String role;
+    private String password;
+    private String fullname;
+    private String avatar;
+    private String mobile;
+    private boolean gender;
+    private String address;
+    private int status;
+    private Date created_at;
+    private Date updated_at;
+
+    public Account() {
+    }  
+
+    public Account(int id, String email, String role, String password, String fullname, String avatar, String mobile, boolean gender, String address, int status, Date created_at, Date updated_at) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.fullname = fullname;
+        this.avatar = avatar;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.address = address;
+        this.status = status;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    } 
+}
